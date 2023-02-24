daaa3c733805016dc56d355e2b9d11722db7a1ee
Akila7302 <bandara98akila@gmail.com>
2023-02-24 20:32:07 +0530

    Made some changes

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import axios from "axios";
 import "../weather.css";
 import SearchBar from "../components/SearchBar";
